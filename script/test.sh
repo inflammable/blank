a872358a73ed2fc4af0a0392440ad0c8049ed2ee
Stuart King <splame@gmail.com>
2021-02-05 23:23:22 +1300

    Add commit artefact

--- script/test.sh
+++ script/test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
+mkdir artefacts
+figlet ${BUILDKITE_COMMIT} > artefacts/commit.txt
+
 EXITCODE=0
-
 if ((RANDOM%2)); then EXITCODE=1; fi
-
 echo "Exit code is ${EXITCODE}"
-
 exit $EXITCODE
 
